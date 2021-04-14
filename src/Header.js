943c9f01b17c2ad61ffeb6f6b1d8e84ab5d898b7
Alphairus <cmcauliffe006@gmail.com>
2021-04-14 18:21:37 -0400

    Logo creation and position

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import Logo from './Track The DIP.png'
 
 function Header() {
     return (
         <div className="header_wrapper">
             {/* logo */}
             <div className="header_logo">
-                TEST
+                <img src={Logo} width={300} />
+                
             </div>
             {/* search */}
             {/* menuitems */}
