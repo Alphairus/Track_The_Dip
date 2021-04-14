12f971420433e766acf6bdabcf4ba94baf532851
Alphairus <cmcauliffe006@gmail.com>
2021-04-14 19:25:44 -0400

    Created newsfeed & stat components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
-import Header from './Header'
+import Header from './Header';
+import Newsfeed from './Newsfeed';
+import Stats from './Stats';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       {/* Header */}
       <div className="app_header">
         <Header />
       </div>
       < div className="app_body">
-
+        <div className="app_container">
+          <Newsfeed />
+          {/* stats */}
+          <Stats />
+        </div>
       </div>
       {/* Body */}
     </div>
---
+++ src/Newsfeed.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Newsfeed.css'
+
+function Newsfeed() {
+    return (
+        <div className="newsfeed">
+            <div className="newsfeed_cointainer">
+
+            </div>
+        </div>
+    )
+}
+
+export default Newsfeed;
