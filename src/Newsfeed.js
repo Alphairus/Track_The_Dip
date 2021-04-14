be01fad4338fbed419c21f24811f98e0d1ba529f
Alphairus <cmcauliffe006@gmail.com>
2021-04-14 19:41:45 -0400

    container style update. prep for chart creation

--- src/Newsfeed.js
+++ src/Newsfeed.js
@@ -5,7 +5,12 @@ function Newsfeed() {
     return (
         <div className="newsfeed">
             <div className="newsfeed_cointainer">
-
+                <div className="newsfeed_chartSection">
+                    <div className="newsfeed_portfolio">
+                        <h1>$114,656.84</h1>
+                        <p>+44.63 (=0.04) Today</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
