f12325fe4363a56eb205f1a4dbfc2d0708ae8c2c
Alphairus <cmcauliffe006@gmail.com>
2021-04-14 16:37:17 -0400

    Header test

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="header_wrapper">
+            {/* logo */}
+            <div className="header_logo">
+                TEST
+            </div>
+            {/* search */}
+            {/* menuitems */}
+        </div>
+    )
+}
+
+export default Header
