5eb1e22596270519701543ded99f832de1ee4634
Alphairus <cmcauliffe006@gmail.com>
2021-04-15 18:06:45 -0400

    Revert "Created newsfeed & stat components"
    This reverts commit 12f971420433e766acf6bdabcf4ba94baf532851.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import './App.css';
-import Header from './Header';
-import Newsfeed from './Newsfeed';
-import Stats from './Stats';
+import Header from './Header'
 
 function App() {
   return (
-    <div className="app">
+    <div className="App">
       {/* Header */}
       <div className="app_header">
         <Header />
       </div>
       < div className="app_body">
-        <div className="app_container">
-          <Newsfeed />
-          {/* stats */}
-          <Stats />
-        </div>
+
       </div>
       {/* Body */}
     </div>
--- src/Header.js
+++ src/Header.js
@@ -1,31 +1,30 @@
-import React from 'react';
-import Logo from './trackthedip.svg';
-import './Header.css';
+import React from 'react'
+import Logo from './Track The DIP.png'
+import './Header.css'
 
 function Header() {
     return (
         <div className="header_wrapper">
             {/* logo */}
             <div className="header_logo">
-                <img src={Logo} alt="" width={25} />
+                <img src={Logo} width={100} />
             </div>
             {/* search */}
             <div className="header_search">
                 <div className="header_searchContainer">
-                    {/* <SearchOutlined /> */}
                     <input placeholder="Search" type="text" />
                 </div>
             </div>
             {/* menuitems */}
             <div className="header_menuItems">
-                <a href="/#">Coins</a>
-                <a href="/#">Portfolio</a>
-                <a href="/#">Cash</a>
-                <a href="/#">Messages</a>
-                <a href="/#">Account</a>
+                <a href="#">Coins</a>
+                <a href="#">Portfolio</a>
+                <a href="#">Cash</a>
+                <a href="#">Messages</a>
+                <a href="#">Account</a>
             </div>
         </div>
-    );
+    )
 }
 
 export default Header
