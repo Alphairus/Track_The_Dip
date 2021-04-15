d1bae91f508327678e48d72f6959a5d4126b69eb
Alphairus <cmcauliffe006@gmail.com>
2021-04-14 22:01:20 -0400

    stock tracking installed firbase

--- src/Stats.js
+++ src/Stats.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Stats.css'
+import axios from "axios";
+
+const TOKEN = "c1rpdjaad3ifb04k9cbg"
+const BASE_URL = "'https://finnhub.io/api/v1/quote"
 
 function Stats() {
+
+    const [stockData, setStockData] = useState([])
+    const [ myStocks, setmyStocks ] = useState([])
+    const getStocksData = (stock) => {
+     return axios
+      .get(`${BASE_URL}${stock}${KEY_URL}`)
+      .catch((error) => {
+        console.error("Error", error.message);
+      });
+  };
+    
+
+    useEffect(()=>{
+
+    }, [])
+
+
     return (
         <div className="stats">
             <div className="stats_container">
