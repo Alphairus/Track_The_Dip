087a87a2724664140f18d5034b0da1df5d365691
Alphairus <cmcauliffe006@gmail.com>
2021-04-16 19:51:28 -0400

    Data being grabbed successfully pre-row creation for the newsfeed container

--- src/Stats.js
+++ src/Stats.js
@@ -1,29 +1,78 @@
-import React from 'react'
-import './Stats.css'
+import React, { useState, useEffect } from "react";
+import "./Stats.css";
+import axios from "axios";
+
+const TOKEN = "c1rpdjaad3ifb04k9cbg";
+const BASE_URL = "https://finnhub.io/api/v1/quote";
 
 function Stats() {
-    return (
-        <div className="stats">
-            <div className="stats_container">
-                <div className="stats_header">
-                    <p>Stocks</p>
-                </div>
-                <div className="stats_content">
-                    <div className="stats_rows">
-{/* for our current stocks */}
-                    </div>
-                </div>
-                <div className="stats_header">
-                    <p>Lists</p>
-                </div>
-                <div className="stats_content">
-                    <div className="stats_rows">
-{/* stocks we can buy */}
-                    </div>
-                </div>
-            </div>
+  const [stockData, setstockData] = useState([]);
+
+  const getStocksData = (stock) => {
+    return axios
+      .get(`${BASE_URL}?symbol=${stock}&token=${TOKEN}`)
+      .catch((error) => {
+        console.error("Error", error.message);
+      });
+  };
+
+  useEffect(() => {
+    let tempStocksData = [];
+    const stocksList = [
+      "AAPL",
+      "MSFT",
+      "TSLA",
+      "FB",
+      "BABA",
+      "UBER",
+      "DIS",
+      "SBUX",
+    ];
+    let promises = [];
+    stocksList.map((stock) => {
+      promises.push(
+        getStocksData(stock).then((res) => {
+          tempStocksData.push({
+            name: stock,
+            ...res.data,
+          });
+        })
+      );
+    });
+
+    Promise.all(promises).then(() => {
+      setstockData(tempStocksData);
+      console.log(tempStocksData);
+    });
+  }, []);
+
+  return (
+    <div className="stats">
+      <div className="stats_container">
+        <div className="stats_header">
+          <p>Stocks</p>
         </div>
-    )
+        <div className="stats_content">
+          <div className="stats_rows"></div>
+        </div>
+        <div className="stats_header">
+          <p>Lists</p>
+        </div>
+        <div className="stats_content">
+          <div className="stats_rows">
+            {stockData.map((stock) => (
+              <StatsRow
+                key={stock.name}
+                name={stock.name}
+                openPrice={stock.o}
+                price={stock.c}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Stats
+export default Stats;
